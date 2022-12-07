38a92d0e6b2ad68cd79bf5f83b8b404a22777f79
Alex Brodnicki <apbrodnicki@gmail.com>
2022-12-07 16:51:15 -0600

    Day 6 (day 5 was making me woozy)

---
+++ 2022/day-6/2.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const startTime = performance.now();
+
+const read = readline.createInterface({
+	input: fs.createReadStream('input.txt'),
+});
+
+let current = [];
+let finalIndex = 0;
+
+read.on('line', function (line) {
+	const array = line.split('');
+
+	array.some((char, index) => {
+		if (current.length === 14) {
+			finalIndex = index;
+			return true;
+		}
+
+		if (!current.includes(char)) {
+			current.push(char);
+		} else {
+			let oldIndex = current.indexOf(char);
+			current = current.slice(oldIndex + 1);
+			current.push(char);
+		}
+	});
+});
+
+read.on('close', function () {
+	console.log({ current, finalIndex });
+	const endTime = ((performance.now() - startTime) / 1000).toFixed(4);
+	console.log('Time to run:', endTime, 'seconds');
+});
